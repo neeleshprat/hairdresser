8912b3132823a9d585110afe052b763da270bbd0
Fabian Kajzar <fabian.kajzar@gmail.com>
2013-10-02 10:14:35 +0200

    Employee Service: Appointment Overview -> dummy implementation

---
+++ backend/src/main/java/de/mannheim/uni/apdtld/teamc/model/Customer.java
@@ -0,0 +1,16 @@
+package de.mannheim.uni.apdtld.teamc.model;
+
+public class Customer {
+
+	private String name;
+
+	public Customer(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
---
+++ backend/src/main/java/de/mannheim/uni/apdtld/teamc/model/Entry.java
@@ -0,0 +1,13 @@
+package de.mannheim.uni.apdtld.teamc.model;
+
+public class Entry {
+	private long start;
+	private long end;
+
+	public Entry(long start, long end) {
+		super();
+		this.start = start;
+		this.end = end;
+	}
+
+}
